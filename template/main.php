bb14feb90176d248da0f22bf77e3e5bbbf46d8bd
Roman <cikipuki10@gmail.com>
2023-12-13 18:57:00 +0200

    update main.php

--- template/main.php
+++ template/main.php
@@ -9,6 +9,23 @@ foreach ($result as $item) {
     $out .= '<img src=/static/images/' . $item['image'] . ' width=200 >'; 
     $out .= '<div><a href=/article/' . $item['url'] . '> Читать полность</a></div>'; 
     $out .= '</div>';
+
 }
 
-echo $out;
+?>
+
+<h1>Main Panel</h1>
+<div><a href="/login">login</a></div>
+<div><a href="/register">register</a></div>
+
+
+<h3>Category</h3>
+<div><a href="/category/movie">movie</a></div>
+<div><a href="/category/cartoon">cartoon</a></div>
+
+
+<?php
+    echo $out;
+?>
+
+    
